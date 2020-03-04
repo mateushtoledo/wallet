22fab2afa52cb94689bd9a69606359c582710152
Mateus Toledo <mateushtoledo@gmail.com>
2020-03-03 22:49:43 -0300

    Removing compiled classes

--- target/classes/db/migration/postgres/V2__fix_tables.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE wallet ADD COLUMN fk_user_id BIGINT;
-UPDATE wallet SET fk_user_id = (SELECT uw.users FROM users_wallet uw WHERE uw.wallet = id);
-ALTER TABLE wallet ADD CONSTRAINT fk_user_id_constraint FOREIGN KEY (fk_user_id) REFERENCES users(id);
-DROP TABLE users_wallet;
